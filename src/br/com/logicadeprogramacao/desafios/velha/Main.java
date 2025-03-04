7c2b2e7ba1efa6f3d7a7baf8df5bfea3a41cf0d6
Natalirodriguess <natalyrodrigues43210@gmail.com>
2025-03-03 23:46:35 -0300

    iniciando novo desafio logica

---
+++ src/br/com/logicadeprogramacao/desafios/velha/Main.java
@@ -0,0 +1,12 @@
+package br.com.logicadeprogramacao.desafios.velha;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                System.out.print(i + "" + j + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+}
